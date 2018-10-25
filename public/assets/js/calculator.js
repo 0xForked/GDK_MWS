13e1a9ce02d3a5ef456e2bb723a660abe90c8438
A. A. Sumitro <aasumitro@merahputih.id>
2018-10-25 22:29:05 +0800

    update

--- public/assets/js/calculator.js
+++ public/assets/js/calculator.js
@@ -1,8 +1,73 @@
-function kalkulator() {
-  let angka = document.querySelectorAll('input');
-  let i1= angka[0].value ; //angka pertama
-  let i2= angka[1].value ; //angka kedua
-  angka[2].value= parseInt(i1) + parseInt(i2) ;
- }
- let tombol = document.querySelector('button');
- tombol.addEventListener('click', kalkulator);
+let result;
+let firstNumb;
+let secondNumb;
+let operatorType;
+
+function operatorListener() {
+  calculator();
+}
+
+function calculator() {
+  if(this.name === 'input1') {
+    firstNumb = this.value;
+  } if(this.name === 'input2') {
+    secondNumb = this.value;
+  }
+
+  var e = document.getElementById("operator");
+  var operator = e.options[e.selectedIndex].value;
+  operatorType =operator
+
+  operation();
+}
+
+function operation() {
+  // addition (+), subtraction(-), multiplication(*)
+  // division(/), modulus(%)
+  if(firstNumb && secondNumb && operatorType) {
+    var result = 0;
+    switch (operatorType) {
+      case 'addition' :
+        result = parseInt(firstNumb) + parseInt(secondNumb);
+      break;
+      case 'subtraction' :
+        result = parseInt(firstNumb) - parseInt(secondNumb);
+      break;
+      case 'division' :
+        result = parseInt(firstNumb) / parseInt(secondNumb);
+      break;
+      case 'multiplication' :
+        result = parseInt(firstNumb) * parseInt(secondNumb);
+      break;
+      case 'modulus' :
+        result = parseInt(firstNumb) % parseInt(secondNumb);
+      break;
+    }
+    document.getElementById('result').innerHTML = result
+  } else {
+    document.getElementById('result').innerHTML = '...'
+  }
+}
+
+const numbCalculate = document.getElementsByTagName('input')
+for (let i = 0; i < numbCalculate.length; i++) {
+  operatorType = '';
+  numbCalculate[i].addEventListener('keyup', calculator, false);
+}
+
+function clear() {
+  document.getElementById('input1').value = '';
+  document.getElementById('input2').value = '';
+  document.getElementById('result').innerHTML = '...';
+  result = '';
+  firstNumb = '';
+  secondNumb = '';
+  operatorType = '';
+}
+
+var onClear = document.getElementById("clear");
+if (onClear.addEventListener) {
+  onClear.addEventListener("click", clear, false);
+} else if (onClear.attachEvent) {
+  onClear.attachEvent('onclick', clear);
+}
--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('map').setView([1.4572787,124.8297592], 16);
+var mymap = L.map('map').setView([1.4644352,124.8290538], 16);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -8,13 +8,20 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(mymap);
 
 let places = [
-  {"lokasi": [1.455512, 124.827119], "sponsor" : "Kantro Pusat UNSRAT"},
-  {"lokasi": [1.456103, 124.827903], "sponsor" : "Kantor PTI UNSRAT"},
-  {"lokasi": [1.459395, 124.828179], "sponsor" : "Kantor LPPM UNSRAT"},
-  {"lokasi": [1.460392, 124.829631], "sponsor" : "Auditorium UNSRAT"},
-  {"lokasi": [1.458786, 124.826212], "sponsor" : "Food Court UNSRAT"}
+  {"lokasi": [1.463571, 124.829470], "pic": "bensu.png", "nama" : "Geprek BENSU", "alamat": "Jalan R.W.Monginsidi No.19 Sario Malalayang, Sario Tumpaan, Sario, Kota Manado, Sulawesi Utara"},
+  {"lokasi": [1.463387, 124.829769], "pic": "mjm.png", "nama" : "RM. MJM", "alamat": "Jl. Wolter Monginsidi No.3, Ranotana, Sario, Sario Tumpaan, Sario, Kota Manado, Sulawesi Utara"},
+  {"lokasi": [1.463193, 124.828400], "pic": "narto.png", "nama" : "Martabak Mas Narto", "alamat": "Jl. Bethesda, Sario Tumpaan, Sario, Kota Manado, Sulawesi Utara"},
+  {"lokasi": [1.463407, 124.828284], "pic": "alibaba.png", "nama" : "Alibaba Coffee House", "alamat": "Jalan Wolter Monginsidi No.42, Sario Tumpaan, Sario, Sario Tumpaan, Sario, Kota Manado, Sulawesi Utara"},
+  {"lokasi": [1.464188, 124.830079], "pic": "sate.png", "nama" : "Sate Madura Sario", "alamat": "Jl. Ahmad Yani, Sario Utara, Sario, Kota Manado, Sulawesi Utara"}
 ];
 
-for (var p of places) {
-  var  marker = L.marker(p.lokasi).addTo(mymap).bindPopup(p.sponsor);
-}
+// for (var p of places) {
+//   var  marker = L.marker(p.lokasi).addTo(mymap).bindPopup(p.nama);
+// }
+
+places.forEach(function(place) {
+  return L.marker(place.lokasi).addTo(mymap).bindPopup(place.nama).on("click", () => {
+    document.getElementById('imagePlace').src = `/assets/img/place/${place.pic}`;
+    document.querySelector('.review').innerHTML = `${place.alamat}`
+  });
+})
