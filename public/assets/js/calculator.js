221b471859ca0ce96246bbb7bd870d70878f194f
Agus Adhi Sumitro <mail@aasumitro.id>
2018-10-29 20:16:32 +0800

    map asyc

--- public/assets/js/calculator.js
+++ public/assets/js/calculator.js
@@ -4,70 +4,70 @@ let secondNumb;
 let operatorType;
 
 function operatorListener() {
-  calculator();
+    calculator();
 }
 
 function calculator() {
-  if(this.name === 'input1') {
-    firstNumb = this.value;
-  } if(this.name === 'input2') {
-    secondNumb = this.value;
-  }
+    if(this.name === 'input1') {
+        firstNumb = this.value;
+    } if(this.name === 'input2') {
+        secondNumb = this.value;
+    }
 
-  var e = document.getElementById("operator");
-  var operator = e.options[e.selectedIndex].value;
-  operatorType =operator
+    var e = document.getElementById("operator");
+    var operator = e.options[e.selectedIndex].value;
+    operatorType = operator
 
-  operation();
+    operation();
 }
 
 function operation() {
-  // addition (+), subtraction(-), multiplication(*)
-  // division(/), modulus(%)
-  if(firstNumb && secondNumb && operatorType) {
-    var result = 0;
-    switch (operatorType) {
-      case 'addition' :
-        result = parseInt(firstNumb) + parseInt(secondNumb);
-      break;
-      case 'subtraction' :
-        result = parseInt(firstNumb) - parseInt(secondNumb);
-      break;
-      case 'division' :
-        result = parseInt(firstNumb) / parseInt(secondNumb);
-      break;
-      case 'multiplication' :
-        result = parseInt(firstNumb) * parseInt(secondNumb);
-      break;
-      case 'modulus' :
-        result = parseInt(firstNumb) % parseInt(secondNumb);
-      break;
+    // addition (+), subtraction(-), multiplication(*)
+    // division(/), modulus(%)
+    if(firstNumb && secondNumb && operatorType) {
+        var result = 0;
+        switch (operatorType) {
+            case 'addition' :
+                result = parseInt(firstNumb) + parseInt(secondNumb);
+            break;
+            case 'subtraction' :
+                result = parseInt(firstNumb) - parseInt(secondNumb);
+            break;
+            case 'division' :
+                result = parseInt(firstNumb) / parseInt(secondNumb);
+            break;
+            case 'multiplication' :
+                result = parseInt(firstNumb) * parseInt(secondNumb);
+            break;
+            case 'modulus' :
+                result = parseInt(firstNumb) % parseInt(secondNumb);
+            break;
+        }
+        document.getElementById('result').innerHTML = result
+    } else {
+        document.getElementById('result').innerHTML = '...'
     }
-    document.getElementById('result').innerHTML = result
-  } else {
-    document.getElementById('result').innerHTML = '...'
-  }
 }
 
 const numbCalculate = document.getElementsByTagName('input')
 for (let i = 0; i < numbCalculate.length; i++) {
-  operatorType = '';
-  numbCalculate[i].addEventListener('keyup', calculator, false);
+    operatorType = '';
+    numbCalculate[i].addEventListener('keyup', calculator, false);
 }
 
 function clear() {
-  document.getElementById('input1').value = '';
-  document.getElementById('input2').value = '';
-  document.getElementById('result').innerHTML = '...';
-  result = '';
-  firstNumb = '';
-  secondNumb = '';
-  operatorType = '';
+    document.getElementById('input1').value = '';
+    document.getElementById('input2').value = '';
+    document.getElementById('result').innerHTML = '...';
+    result = '';
+    firstNumb = '';
+    secondNumb = '';
+    operatorType = '';
 }
 
 var onClear = document.getElementById("clear");
 if (onClear.addEventListener) {
-  onClear.addEventListener("click", clear, false);
+    onClear.addEventListener("click", clear, false);
 } else if (onClear.attachEvent) {
-  onClear.attachEvent('onclick', clear);
+    onClear.attachEvent('onclick', clear);
 }
--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -9,21 +9,58 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiYXN1bWl0cm8iLCJhIjoiY2oxaHM2bW9jMDBlazMycGpkYXExNDB2NSJ9.OF9SabdurZG8_v3UZOWe0A'
 }).addTo(mymap);
 
-let places = [
-  {"lokasi": [1.463571, 124.829470], "pic": "bensu.png", "nama" : "Geprek BENSU", "alamat": "Jalan R.W.Monginsidi No.19 Sario Malalayang, Sario Tumpaan, Sario, Kota Manado, Sulawesi Utara"},
-  {"lokasi": [1.463387, 124.829769], "pic": "mjm.png", "nama" : "RM. MJM", "alamat": "Jl. Wolter Monginsidi No.3, Ranotana, Sario, Sario Tumpaan, Sario, Kota Manado, Sulawesi Utara"},
-  {"lokasi": [1.463193, 124.828400], "pic": "narto.png", "nama" : "Martabak Mas Narto", "alamat": "Jl. Bethesda, Sario Tumpaan, Sario, Kota Manado, Sulawesi Utara"},
-  {"lokasi": [1.463407, 124.828284], "pic": "alibaba.png", "nama" : "Alibaba Coffee House", "alamat": "Jalan Wolter Monginsidi No.42, Sario Tumpaan, Sario, Sario Tumpaan, Sario, Kota Manado, Sulawesi Utara"},
-  {"lokasi": [1.464188, 124.830079], "pic": "sate.png", "nama" : "Sate Madura Sario", "alamat": "Jl. Ahmad Yani, Sario Utara, Sario, Kota Manado, Sulawesi Utara"}
-];
+// let places = [
+//   {"lokasi": [1.463571, 124.829470], "pic": "bensu.png", "nama" : "Geprek BENSU", "alamat": "Jalan R.W.Monginsidi No.19 Sario Malalayang, Sario Tumpaan, Sario, Kota Manado, Sulawesi Utara"},
+//   {"lokasi": [1.463387, 124.829769], "pic": "mjm.png", "nama" : "RM. MJM", "alamat": "Jl. Wolter Monginsidi No.3, Ranotana, Sario, Sario Tumpaan, Sario, Kota Manado, Sulawesi Utara"},
+//   {"lokasi": [1.463193, 124.828400], "pic": "narto.png", "nama" : "Martabak Mas Narto", "alamat": "Jl. Bethesda, Sario Tumpaan, Sario, Kota Manado, Sulawesi Utara"},
+//   {"lokasi": [1.463407, 124.828284], "pic": "alibaba.png", "nama" : "Alibaba Coffee House", "alamat": "Jalan Wolter Monginsidi No.42, Sario Tumpaan, Sario, Sario Tumpaan, Sario, Kota Manado, Sulawesi Utara"},
+//   {"lokasi": [1.464188, 124.830079], "pic": "sate.png", "nama" : "Sate Madura Sario", "alamat": "Jl. Ahmad Yani, Sario Utara, Sario, Kota Manado, Sulawesi Utara"}
+// ];
 
 // for (var p of places) {
 //   var  marker = L.marker(p.lokasi).addTo(mymap).bindPopup(p.nama);
 // }
 
-places.forEach(function(place) {
-  return L.marker(place.lokasi).addTo(mymap).bindPopup(place.nama).on("click", () => {
-    document.getElementById('imagePlace').src = `/assets/img/place/${place.pic}`;
-    document.querySelector('.review').innerHTML = `${place.alamat}`
-  });
-})
+// places.forEach(function(place) {
+//   return L.marker(place.lokasi).addTo(mymap).bindPopup(place.nama).on("click", () => {
+//     document.getElementById('imagePlace').src = `/assets/img/place/${place.pic}`;
+//     document.querySelector('.review').innerHTML = `${place.alamat}`
+//   });
+// })
+
+// fetch(URL)
+// .then(function(response){
+//     if (!response.ok) { //HTTP Status
+//         console.log('Ada masalah. Status Code: ' + response.status);
+//         throw response.statusText;
+//     }
+//     return response.json()
+// })
+// .then(function(res){
+//      markPlaces(res);
+// })
+// .catch(function(err){
+//     console.log(err);
+// });
+
+async function getPlaces(url){
+    try {
+        const placeUrl = await(fetch(url));
+        const placeJson = await placeUrl.json();
+        markPlaces(placeJson);
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+function markPlaces(place) {
+    place.forEach(place => {
+        return L.marker(place.lokasi).addTo(mymap).bindPopup(place.nama).on("click", () => {
+            document.getElementById('imagePlace').src = `/assets/img/place/${place.pic}`;
+            document.querySelector('.review').innerHTML = `${place.alamat}`
+        });
+    })
+}
+
+getPlaces(URL);
